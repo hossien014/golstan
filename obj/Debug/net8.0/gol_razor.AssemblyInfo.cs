c88558b2661238fd17fb7a29b5cf9ed54c8e94e1
hossien <94741775+hossien014@users.noreply.github.com>
2024-05-28 13:46:17 +0330

    disable https redirect

--- obj/Debug/net8.0/gol_razor.AssemblyInfo.cs
+++ obj/Debug/net8.0/gol_razor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gol_razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84678220e2faed9292c61e7c0015198586dc6e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b16af5c1156d7d5f7484ccdefd912b419bebcb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("gol_razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gol_razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
